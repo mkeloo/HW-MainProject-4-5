e384e5fa6e61cc512530a3c2cf95a032de75a547
mkeloo <mkeloo@ufl.edu>
2023-11-29 18:55:36 -0500

    updated imageOps file.

--- src/edu/ufl/cise/cop4020fa23/runtime/ImageOps.java
+++ src/edu/ufl/cise/cop4020fa23/runtime/ImageOps.java
@@ -1,11 +1,11 @@
 /*Copyright 2023 by Beverly A Sanders
- * 
- * This code is provided for solely for use of students in COP4020 Programming Language Concepts at the 
- * University of Florida during the spring semester 2023 as part of the course project.  
- * 
- * No other use is authorized. 
- * 
- * This code may not be posted on a public web site either during or after the course.  
+ *
+ * This code is provided for solely for use of students in COP4020 Programming Language Concepts at the
+ * University of Florida during the spring semester 2023 as part of the course project.
+ *
+ * No other use is authorized.
+ *
+ * This code may not be posted on a public web site either during or after the course.
  */
 
 package edu.ufl.cise.cop4020fa23.runtime;
@@ -25,14 +25,14 @@ import java.util.Arrays;
  * getRGB(int x, int y), which returns an int, and setRGB(int x, int y, int
  * rgb); In these methods, pixels are represented as an int that encodes the 3
  * colors plus the alpha (transparency) value.
- * 
+ *
  * In PLCLang, we do not deal with the alpha value--whenever required, it is set
  * to 0xff.
- * 
+ *
  * In an image, the color of a pixel is packed into an int, where 8 bits are
  * available for the alpha, red, green, and blue components. See the PixelOps 
  * class for methods for working with pixels.
- * 
+ *
  */
 
 public class ImageOps {
@@ -43,14 +43,14 @@ public class ImageOps {
 	/**
 	 * Returns a new image containing only the red component of the given image.
 	 * This method can be used to implement the red channel selector applied to an image.
-	 * 
+	 *
 	 * @param image
 	 * @return
 	 */
 	public static BufferedImage extractRed(BufferedImage image) {
 		int width = image.getWidth();
 		int height = image.getHeight();
-		BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
 				int pixel = getRGB(image, x, y);
@@ -65,8 +65,8 @@ public class ImageOps {
 	/**
 	 * Returns a new image containing only the green component of the given image.
 	 * This can be used to implement the grn  channel selector applied to an image.
-	 * 
-	 * 
+	 *
+	 *
 	 * @param image
 	 * @return
 	 */
@@ -88,8 +88,8 @@ public class ImageOps {
 	/**
 	 * Returns a new image containing only the blue component of the given image.
 	 * This can be used to implement the blu channel selector applied to an image.
-	 * 
-	 * 
+	 *
+	 *
 	 * @param image
 	 * @return
 	 */
@@ -126,51 +126,51 @@ public class ImageOps {
 		int rred = PixelOps.red(rightPacked);
 		int rgrn = PixelOps.green(rightPacked);
 		int rblu = PixelOps.blue(rightPacked);
-		return 
-		switch(op) {
-		case PLUS -> PixelOps.pack(lred + rred, lgrn + rgrn, lblu + rblu);
-		case MINUS -> PixelOps.pack(lred - rred, lgrn - rgrn, lblu - rblu);
-		case TIMES -> PixelOps.pack(lred * rred, lgrn * rgrn, lblu * rblu);
-		case DIV -> PixelOps.pack(lred / rred, lgrn / lgrn, lblu / rblu);
-		case MOD -> PixelOps.pack(lred % rred, lgrn % lgrn, lblu % rblu);
-		default -> throw new IllegalArgumentException("Compiler/runtime error Unexpected value: " + op);
-		};		
+		return
+				switch(op) {
+					case PLUS -> PixelOps.pack(lred + rred, lgrn + rgrn, lblu + rblu);
+					case MINUS -> PixelOps.pack(lred - rred, lgrn - rgrn, lblu - rblu);
+					case TIMES -> PixelOps.pack(lred * rred, lgrn * rgrn, lblu * rblu);
+					case DIV -> PixelOps.pack(lred / rred, lgrn / lgrn, lblu / rblu);
+					case MOD -> PixelOps.pack(lred % rred, lgrn % lgrn, lblu % rblu);
+					default -> throw new IllegalArgumentException("Compiler/runtime error Unexpected value: " + op);
+				};
 	}
-	
+
 	public static int binaryPackedPixelScalarOp(OP op, int leftPacked, int right) {
 		int lred = PixelOps.red(leftPacked);
 		int lgrn = PixelOps.green(leftPacked);
 		int lblu = PixelOps.blue(leftPacked);
-		return 
-		switch(op) {
-		case PLUS -> PixelOps.pack(lred + right, lgrn + right, lblu + right);
-		case MINUS -> PixelOps.pack(lred - right, lgrn - right, lblu - right);
-		case TIMES -> PixelOps.pack(lred * right, lgrn * right, lblu * right);
-		case DIV -> PixelOps.pack(lred / right, lgrn / right, lblu / right);
-		case MOD -> PixelOps.pack(lred % right, lgrn % right, lblu % right);
-		default -> throw new IllegalArgumentException("Compiler/runtime error Unexpected value: " + op);
-		};				
+		return
+				switch(op) {
+					case PLUS -> PixelOps.pack(lred + right, lgrn + right, lblu + right);
+					case MINUS -> PixelOps.pack(lred - right, lgrn - right, lblu - right);
+					case TIMES -> PixelOps.pack(lred * right, lgrn * right, lblu * right);
+					case DIV -> PixelOps.pack(lred / right, lgrn / right, lblu / right);
+					case MOD -> PixelOps.pack(lred % right, lgrn % right, lblu % right);
+					default -> throw new IllegalArgumentException("Compiler/runtime error Unexpected value: " + op);
+				};
 	}
-	
-	
+
+
 	public static boolean binaryPackedPixelBooleanOp(BoolOP op, int left, int right) {
 		int leftNoAlpha = left & ~PixelOps.SELECT_ALPHA;  //zero out alpha component before comparison.
 		int rightNoAlpha = right & ~PixelOps.SELECT_ALPHA;
 		return (op == BoolOP.EQUALS) ? leftNoAlpha == rightNoAlpha : leftNoAlpha != rightNoAlpha;
 	}
-		
+
 	/**
 	 * Returns a new BufferedImage obtained by applying the given binary operator
 	 * to each color component in each pixel in the given images.
-	 * 
+	 *
 	 * If the images do not have the same shape, a PLCRuntimeException is thrown.
-	 * 
+	 *
 	 * @param op
 	 * @param left
 	 * @param right
 	 * @return
 	 */
-	
+
 	public static BufferedImage binaryImageImageOp(OP op, BufferedImage left, BufferedImage right) {
 		int lwidth = left.getWidth();
 		int rwidth = right.getWidth();
@@ -190,8 +190,8 @@ public class ImageOps {
 		}
 		return result;
 	}
-	
-	
+
+
 	public static BufferedImage binaryImagePixelOp(OP op, BufferedImage left, int right) {
 		int lwidth = left.getWidth();
 		int lheight = left.getHeight();
@@ -206,11 +206,11 @@ public class ImageOps {
 		}
 		return result;
 	}
-	
+
 	/**
-	 * Returns a new buffered image obtained by applying the given binary operation 
+	 * Returns a new buffered image obtained by applying the given binary operation
 	 * to each color component in each pixel in the given image (left) and the int value (right).
-	 * 
+	 *
 	 * @param op
 	 * @param left
 	 * @param right
@@ -229,9 +229,9 @@ public class ImageOps {
 		}
 		return result;
 	}
-	
 
-	
+
+
 	public static BufferedImage setAllPixels(BufferedImage image, int packed) {
 		for (int x = 0; x < image.getWidth(); x++)
 			for (int y = 0; y < image.getHeight(); y++) {
@@ -240,10 +240,10 @@ public class ImageOps {
 		return image;
 	}
 
-	
+
 	/**
-	 * Creates an image of given size. 
-	 * 
+	 * Creates an image of given size.
+	 *
 	 * @param width
 	 * @param height
 
@@ -252,49 +252,78 @@ public class ImageOps {
 	public static BufferedImage makeImage(int width, int height) {
 		return new BufferedImage(width, height,BufferedImage.TYPE_INT_RGB);
 	}
-	
+
 	/**
-	 * Returns a new image that is copy of the given BufferedImage 
+	 * Returns a new image that is copy of the given BufferedImage
 	 * @param image
 	 * @return new image that is copy of the given image
 	 */
 	public static final BufferedImage cloneImage(BufferedImage image) {
-	    BufferedImage clone = new BufferedImage(image.getWidth(),
-	            image.getHeight(), image.getType());
-	    Graphics2D g2d = clone.createGraphics();
-	    g2d.drawImage(image, 0, 0, null);
-	    g2d.dispose();
-	    return clone;
+		BufferedImage clone = new BufferedImage(image.getWidth(),
+				image.getHeight(), image.getType());
+		Graphics2D g2d = clone.createGraphics();
+		g2d.drawImage(image, 0, 0, null);
+		g2d.dispose();
+		return clone;
 	}
-	
+
 
 	/**
 	 * Copies one image into the other, resizing to fit the size of the destination image
-	 * 
+	 *
 	 * @param sourceImage
 	 * @param destImage
 	 */
-	public static final void copyInto(BufferedImage sourceImage, BufferedImage destImage) {		
-        int w = sourceImage.getWidth();
-		int h = sourceImage.getHeight();
-		int maxX = destImage.getWidth();
-		int maxY = destImage.getHeight();
-		AffineTransform at = new AffineTransform();
-		at.scale(((float) maxX) / w, ((float) maxY) / h);
-		AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-		scaleOp.filter(sourceImage, destImage);
+	public static final void copyInto(BufferedImage sourceImage, BufferedImage destImage) {
+		int destWidth = destImage.getWidth();
+		int destHeight = destImage.getHeight();
+//		BufferedImage resizedImage = null;
+//		if (destWidth != sourceImage.getWidth() || destHeight != sourceImage.getHeight()) {
+//		  resizedImage = copyAndResize(sourceImage,destWidth, destHeight);
+//		}
+//		else {
+//			resizedImage = destImage;
+//		}
+//		copyIntoNoResize(resizedImage, destImage);
+
+		if (destWidth != sourceImage.getWidth() || destHeight != sourceImage.getHeight()) {
+			copyIntoNoResize(copyAndResize(sourceImage,destWidth, destHeight), destImage);
+		}
+		else {
+			copyIntoNoResize(sourceImage, destImage);
+		}
+
 	}
-	
+
+	/*Copies the pixels of the soureImage into the pixels of the destImage
+	 *
+	 * Precondition:  sourceImage and destImage are the same size
+	 * This routine is not public.  Generated code should use copyInto.
+	 */
+	static void copyIntoNoResize(BufferedImage sourceImage, BufferedImage destImage) {
+		int w = sourceImage.getWidth();
+		int h = sourceImage.getHeight();
+		int destWidth = destImage.getWidth();
+		int destHeight = destImage.getHeight();
+		if ( (w != destWidth) || h != destHeight) throw new PLCRuntimeException("copyIntoNoResize source and destination image have different sizes");
+		for (int y = 0; y < destHeight; y++) {
+			for (int x = 0; x < destWidth; x++) {
+				int pixel = sourceImage.getRGB(x, y);
+				destImage.setRGB(x, y, pixel);
+			}
+		}
+	}
+
 	/**
 	 * Returns a new image that is a resized version of the 'before' image.
-	 * 
+	 *
 	 * @param image
 	 * @param maxX
 	 * @param maxY
 	 * @return new image that is a resized version of the 'before' image
 	 */
 	public static BufferedImage copyAndResize(BufferedImage image, int maxX,
-			int maxY) {
+											  int maxY) {
 		int w = image.getWidth();
 		int h = image.getHeight();
 		AffineTransform at = new AffineTransform();
@@ -304,27 +333,27 @@ public class ImageOps {
 		newResizedImage = scaleOp.filter(image, newResizedImage);
 		return newResizedImage;
 	}
-	
+
 
 	/**
 	 * Returns an array of ints representing the packed pixels of the given image.
-	 * 
+	 *
 	 * This can be used in Junit test to compare two images by using 
 	 * assertArrayEquals on the array return from this method.
-	 * 
+	 *
 	 * @param result
 	 * @return array of ints representing the packed pixels of the given image
 	 */
 	public static int[] getRGBPixels(BufferedImage result) {
 		return result.getRGB(0,0,result.getWidth(), result.getHeight(), null,0,result.getWidth());
 	}
-	
+
 	public static boolean equals(BufferedImage image0, BufferedImage image1) {
 		int[] pixels0 = getRGBPixels(image0);
 		int[] pixels1 = getRGBPixels(image1);
 		return Arrays.equals(pixels0, pixels1);
 	}
-	
+
 	/**
 	 * If coordinates are out of bounds does nothing. 
 	 */
@@ -335,7 +364,7 @@ public class ImageOps {
 			image.setRGB(x, y, pixel);
 		}
 	}
-		
+
 	/**
 	 * If coordinates are out of bounds it returns a black pixel. 
 	 */
@@ -355,15 +384,15 @@ public class ImageOps {
 		int rred = q;
 		int rgrn = q;
 		int rblu = q;
-		return 
-		switch(op) {
-		case PLUS -> PixelOps.pack(lred + rred, lgrn + rgrn, lblu + rblu);
-		case MINUS -> PixelOps.pack(lred - rred, lgrn - rgrn, lblu - rblu);
-		case TIMES -> PixelOps.pack(lred * rred, lgrn * rgrn, lblu * rblu);
-		case DIV -> PixelOps.pack(lred / rred, lgrn / rgrn, lblu / rblu);
-		case MOD -> PixelOps.pack(lred % rred, lgrn % rgrn, lblu % rblu);
-		default -> throw new IllegalArgumentException("Compiler/runtime error Unexpected value: " + op);
-		};		
+		return
+				switch(op) {
+					case PLUS -> PixelOps.pack(lred + rred, lgrn + rgrn, lblu + rblu);
+					case MINUS -> PixelOps.pack(lred - rred, lgrn - rgrn, lblu - rblu);
+					case TIMES -> PixelOps.pack(lred * rred, lgrn * rgrn, lblu * rblu);
+					case DIV -> PixelOps.pack(lred / rred, lgrn / rgrn, lblu / rblu);
+					case MOD -> PixelOps.pack(lred % rred, lgrn % rgrn, lblu % rblu);
+					default -> throw new IllegalArgumentException("Compiler/runtime error Unexpected value: " + op);
+				};
 	}
-		
-	}
+
+}
